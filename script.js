67db422558e1e7e4eab6fa752c3de6de710b46b5
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function highlight() {
-    //Write your code here
-
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
+document.addEventListener('DOMContentLoaded', function() {
+    const highlightLink = document.getElementById('highlight-link');
+    const strongElements = document.querySelectorAll('strong');
     
-}
+    // Store original colors
+    const originalColors = [];
+    strongElements.forEach(el => {
+        originalColors.push({
+            element: el,
+            color: window.getComputedStyle(el).color
+        });
+    });
+    
+    // Highlight function
+    function highlight() {
+        strongElements.forEach(el => {
+            el.style.color = 'rgb(0, 128, 0)';
+        });
+    }
+    
+    // Return to normal function
+    function return_normal() {
+        originalColors.forEach(item => {
+            item.element.style.color = item.color;
+        });
+    }
+    
+    // Event listeners
+    highlightLink.addEventListener('mouseover', highlight);
+    highlightLink.addEventListener('mouseout', return_normal);
+});
